db1865d19be44a50f36634ff696063942f57d9c1
Ikramul-m <i20727172@gmail.com>
2024-04-19 11:03:44 +0530

    Cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./About";
 import Products from "./Products";
 import Contact from "./Contact";
 import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
