091ad84c4fdb38235cdf39d0b94140e628d2d5e5
Ikramul-m <i20727172@gmail.com>
2024-04-19 10:53:57 +0530

    Products

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
+import Products from "./Products";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
   );
