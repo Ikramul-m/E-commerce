cba1e99b63a7cb2f8a3fa0c85f39decdbaa37d7e
Ikramul-m <i20727172@gmail.com>
2024-04-19 10:45:42 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 const App = () => {
-  return <div>Hello! Welcome to Ikram React E-Commerce</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
