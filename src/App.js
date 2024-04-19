cd8bce9206661ebd9c65bad8ff5b9561ae5ab0c2
Ikramul-m <i20727172@gmail.com>
2024-04-19 10:56:01 +0530

    Contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Products from "./Products";
+import Contact from "./Contact";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
